3c59776f5f954d90b94071a7ccc53b3254fbca02
Goncalo Mira <goncalomira99@gmail.com>
2020-08-07 19:02:20 +0100

    Fetch from mysql

--- src/components/ScoreMenu/ScoreMenu.jsx
+++ src/components/ScoreMenu/ScoreMenu.jsx
@@ -1,68 +1,98 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./ScoreMenu.css";
 
-export const ScoreMenu = ({goToMainMenu}) => {
+export const ScoreMenu = ({ goToMainMenu }) => {
+  const [scores, setScores] = useState([]);
   const [dificultyScore, setDificultyScore] = useState("Easy");
   const dificulties = ["Easy", "Medium", "Hard"];
-  const [fetchingScores, setFetchingScores] = useState(true);
+  const [fetchingScores, setFetchingScores] = useState(false);
+  const [fetched, setFetched] = useState(false);
 
   const changeScoreDificulty = (e) => {
     setDificultyScore(e);
   };
 
+  const fetchScores = () => {
+    setFetchingScores(true);
+    axios
+      .get("http://94.46.171.95/test")
+      .then((res) => {
+        setScores(res.data);
+      })
+      .then(setFetchingScores(false));
+  };
+
+  useEffect(() => {
+    fetchScores();
+  }, [fetchingScores]);
+
   return (
-      <>
-    <div className="score-menu">
-      <div className="score-title">Scoreboard</div>
-      <div className="score-dificulties">
-        {dificulties.map((dificulty, index) => {
-          return (
-            <div
-              key={index}
-              className="score-dificulty-button"
-              onClick={() => changeScoreDificulty(dificulty)}
-              style={{
-                color: dificulty === dificultyScore && "red",
-                borderTop:
-                  dificulty !== dificultyScore
-                    ? "2px solid white"
-                    : "2px solid #7d7d7d",
-                borderLeft:
-                  dificulty !== dificultyScore
-                    ? "2px solid white"
-                    : "2px solid #7d7d7d",
-                borderRight:
-                  dificulty !== dificultyScore
-                    ? "2px solid #707070"
-                    : "2px solid #7d7d7d",
-                borderBottom:
-                  dificulty !== dificultyScore
-                    ? "2px solid #707070"
-                    : "2px solid #7d7d7d",
-                backgroundColor: "#bdbdbd",
-                fontSize: "1.5rem",
-              }}
-            >
-              {dificulty}
-            </div>
-          );
-        })}
+    <>
+      <div className="score-menu">
+        <div className="score-title" onClick={fetchScores}>
+          Scoreboard
+        </div>
+        <div className="score-dificulties">
+          {dificulties.map((dificulty, index) => {
+            return (
+              <div
+                key={index}
+                className="score-dificulty-button"
+                onClick={() => changeScoreDificulty(dificulty)}
+                style={{
+                  color: dificulty === dificultyScore && "red",
+                  borderTop:
+                    dificulty !== dificultyScore
+                      ? "2px solid white"
+                      : "2px solid #7d7d7d",
+                  borderLeft:
+                    dificulty !== dificultyScore
+                      ? "2px solid white"
+                      : "2px solid #7d7d7d",
+                  borderRight:
+                    dificulty !== dificultyScore
+                      ? "2px solid #707070"
+                      : "2px solid #7d7d7d",
+                  borderBottom:
+                    dificulty !== dificultyScore
+                      ? "2px solid #707070"
+                      : "2px solid #7d7d7d",
+                  backgroundColor: "#bdbdbd",
+                  fontSize: "1.5rem",
+                }}
+              >
+                {dificulty}
+              </div>
+            );
+          })}
+        </div>
+        <div className="scoreboard">
+          {fetchingScores ? (
+            <div className="fetching-image"></div>
+          ) : (
+            <table>
+              <tr>
+                <th># Place</th>
+                <th>User Name</th>
+                <th>Time [s]</th>
+              </tr>
+              {scores.map((score, index) => {
+                return (
+                  <tr>
+                    <th>{index}</th>
+                    <th>{score.user}</th>
+                    <th>{score.time}</th>
+                  </tr>
+                );
+              })}
+            </table>
+          )}
+        </div>
       </div>
-      <div className="scoreboard">
-        {fetchingScores ? (
-          <div className="fetching-image"></div>
-        ) : (
-          <table>
-            <tr>
-              <th># Place</th>
-              <th>User Name</th>
-              <th>Time [s]</th>
-            </tr>
-          </table>
-        )}
+      <div className="menu-button" onClick={goToMainMenu}>
+        Main Menu
       </div>
-    </div>
-    <div className="menu-button" onClick={goToMainMenu}>Main Menu</div>
     </>
   );
 };
