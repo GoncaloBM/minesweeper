9241421c20a70db58200c2ce359b52d89459745f
Goncalo Mira <goncalomira99@gmail.com>
2020-08-11 19:59:17 +0100

    Updates

---
+++ precache-manifest.2e63967b5bc8c3251e2102cf7401a15b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e785c7298a0a32a70ce2e3df54d56c",
+    "url": "/GoncaloBM/minesweeper/index.html"
+  },
+  {
+    "revision": "e65653e7e7c097c329da",
+    "url": "/GoncaloBM/minesweeper/static/css/main.73342392.chunk.css"
+  },
+  {
+    "revision": "12b659d04e3665f00ae8",
+    "url": "/GoncaloBM/minesweeper/static/js/2.c1fe6e06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GoncaloBM/minesweeper/static/js/2.c1fe6e06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e65653e7e7c097c329da",
+    "url": "/GoncaloBM/minesweeper/static/js/main.42ce0fb0.chunk.js"
+  },
+  {
+    "revision": "30506fd991948f8d0da0",
+    "url": "/GoncaloBM/minesweeper/static/js/runtime-main.8d0653c0.js"
+  },
+  {
+    "revision": "964111990ca3937960147b80e8d07e61",
+    "url": "/GoncaloBM/minesweeper/static/media/Arcade.96411199.TTF"
+  },
+  {
+    "revision": "9d65f4e610da59ad14754d800a3cc41b",
+    "url": "/GoncaloBM/minesweeper/static/media/Crashed Scoreboard.9d65f4e6.ttf"
+  },
+  {
+    "revision": "c335dbc31e3772eaf5f1a76f7684f174",
+    "url": "/GoncaloBM/minesweeper/static/media/Instructions-1.c335dbc3.png"
+  },
+  {
+    "revision": "bb4229ad05b67c6e961c1057252f747e",
+    "url": "/GoncaloBM/minesweeper/static/media/Instructions-2.bb4229ad.png"
+  },
+  {
+    "revision": "e88d3e1241d23887c9bba2b0b78f9a50",
+    "url": "/GoncaloBM/minesweeper/static/media/Instructions-3.e88d3e12.png"
+  },
+  {
+    "revision": "3eb3cba012496a1cf05c8f9884dba9d1",
+    "url": "/GoncaloBM/minesweeper/static/media/Instructions-4.3eb3cba0.png"
+  },
+  {
+    "revision": "86fb13c30f904f9bb4a94aae417accaf",
+    "url": "/GoncaloBM/minesweeper/static/media/Instructions-5.86fb13c3.png"
+  },
+  {
+    "revision": "fde61be7757d293084e4e6752289643c",
+    "url": "/GoncaloBM/minesweeper/static/media/Instructions-7.fde61be7.png"
+  },
+  {
+    "revision": "88c1bb234085705488300af0dba466e2",
+    "url": "/GoncaloBM/minesweeper/static/media/Instructions.88c1bb23.png"
+  },
+  {
+    "revision": "b4b0e779beffa61c881f3e6d23e8acee",
+    "url": "/GoncaloBM/minesweeper/static/media/back-icon.b4b0e779.png"
+  },
+  {
+    "revision": "e9cd5ec463222f03bb0ff8243d55ffb6",
+    "url": "/GoncaloBM/minesweeper/static/media/game_over.e9cd5ec4.ttf"
+  },
+  {
+    "revision": "f6cd1e3456e14a4c187b537f116a5bd3",
+    "url": "/GoncaloBM/minesweeper/static/media/loading.f6cd1e34.gif"
+  },
+  {
+    "revision": "ed80e29a60840c6ebb40e079ca5e7af4",
+    "url": "/GoncaloBM/minesweeper/static/media/nocontinue.ed80e29a.ttf"
+  },
+  {
+    "revision": "72cdc278340abfdce95ce90589b9ea28",
+    "url": "/GoncaloBM/minesweeper/static/media/playgame.72cdc278.otf"
+  }
+]);
